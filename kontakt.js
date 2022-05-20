76fe8bc17bba7a371f52cb869f39be4bcd588b13
Victoria Rønne <victoria.ronne@gmail.com>
2022-05-20 20:38:56 +0200

    Færdiggjort JavaScript

--- kontakt.js
+++ kontakt.js
@@ -4,56 +4,28 @@ const templateID = "template_s2qra5u"
 
 function sendEmail() {
     try {
-        const name = document.getElementById("name").value
-        const email = document.getElementById("email").value
-        const message = document.getElementById("message").value
+        var name = document.getElementById("name")
+        var email = document.getElementById("email")
+        var message = document.getElementById("message")
         const emailData = {
-            name: name,
-            email: email,
-            message: message,
+            name: name.value,
+            email: email.value,
+            message: message.value,
         }
 
+        var infoMessage = document.getElementsByClassName("info-message")[0];
         if (name && email && message) {
             emailjs.send(serviceID, templateID, emailData, userID);
-            (console.log)('Email Sent!')
+            infoMessage.textContent = 'Tak for din mail!';
+            name.value = null;
+            email.value = null
+            message.value = null
+            console.log(name);
         } else {
-            console.log('INVALID - Email NOT Sent!')
+            infoMessage.textContent = 'Udfyld venligst alle felter';
         }
 
     } catch (error) {
-
-        console.log(error)
+        console.log(error);
     }
-
-}
-
-document
-    .getElementById("kontakt")
-    .addEventListener("submit", validateform)
-
-const templateParams = {};
-
-function validateform(event){
-    event.preventDefault();
-    console.log("submitted");
-    const elements = document.getElementsByClassName('need-validation');
-    console.log(elements)
-    for(let element of elements){
-        if (element.type =="text" || element.type == "textarea") {
-            textvalidation(element);
-        } else if (element.type == "email") {
-            emailValidation(element);
-        }
-    }
-}
-
-const errorElements = document.getElementsByClassName("validaiont-error")
-console.log("errorElements", errorElements);
-if (errorElements.length == 0) {
-    emailjs.send(
-        "service_xb1o8hg",
-        "template_s2qra5u",
-        templateParams,
-        "ufO0_wig4VkETaUTU"
-    );
 }
